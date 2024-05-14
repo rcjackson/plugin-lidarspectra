bfa7ad362dffb94c084b3a51dc0e618aa7eb396b
Bobby Jackson <rjackson@anl.gov>
2024-05-14 00:08:53 +0000

    ADD: Latest changes to processing pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 FROM waggle/plugin-base:1.1.1-base
 
 WORKDIR /app
-
 RUN apt-get update -y
 RUN apt update -y
 RUN apt-get install -y --no-install-recommends python3-netcdf4 gcc
 RUN apt-get install -y libpython3-dev
-COPY . . 
+COPY requirements.txt /app
 RUN git config --global --add safe.directory /app
 RUN pip3 install -r requirements.txt
 RUN pip3 install git+https://github.com/rcjackson/HighIQ.git
-
+RUN apt-get install -y libhdf5-serial-dev python3-h5py
+RUN pip3 install h5netcdf
+RUN pip3 install numpy==1.21
+COPY . . 
 
 ENTRYPOINT ["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import highiq
 import xarray as xr
 import os
-import pywaggle
+import paramiko
+import time
 from waggle.plugin import Plugin
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 file_list = ['aet_RHI_240_20230815_060722.raw']
 for lidar_file in file_list:
     homepoint = 'atmos.homepoint'
-    print(lidar_file)
+    logging.debug(lidar_file)
+    start_time = time.time()
     raw_file = highiq.io.read_00_data(lidar_file, homepoint, start_sample=3, end_sample=120)
+    logging.debug(len(raw_file.time))
     my_list = []
-    if len(raw_file.time.values) > 2000:
-        for x in raw_file.groupby_bins('time', raw_file.time.values[::2000]):
-            d = x[1]
-            psd = highiq.calc.get_psd(d, nfft=128,
-                    time_window=10.)
-            my_list.append(highiq.calc.get_lidar_moments(psd))
-            del psd
-        ds_out = xr.concat(my_list, dim='time')
-        # Improper noise floor at some
-    else:
-        ds_out = highiq.calc.get_psd(d, nfft=128, time_window=10.)
-        ds_out = highiq.calc.get_lidar_moments(ds_out)
-    ds.to_netcdf(lidar_file + '.nc', engine='h5netcdf')
+    logging.debug("Processing spectra")
+    ds_out = highiq.calc.get_psd(raw_file, nfft=256)
+    logging.debug("Output spectra")
+    ds_out = highiq.calc.get_lidar_moments(ds_out)
+    ds_out.to_netcdf(lidar_file + '.nc', engine='h5netcdf')
+
     with Plugin() as plugin:
         plugin.upload_file(lidar_file + '.nc')
+    logging.debug("Time taken: %f" % (time.time() - start_time))
